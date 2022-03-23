b5e0ff65d2522683b32e072204b6037f5957fcc5
ptuan23050@gmail.com <ptuan23050@gmail.com>
2022-03-23 15:12:38 +0700

    Release suntech frontend final project

---
+++ iu nghi.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Bé ơiii~, bé đồng ý làm vợ anh nha :')",
+  descriptionHeader:
+    "Nếu bé thoát ra và không trả lời thì có nghĩa là bé đã ĐỒNG Ý rồi nhá ^^",
+  buttonYes: "Em Đồng Ý ạ :3",
+  buttonNo: "Hok póe ơii ^^",
+  titleModar: "Anh biết bé sẽ đồng ý mà :3 </br> 26-3 anh tới rước dâu nhé~ ",
+  descriptionModar:
+    'Anh biết là bé sẽ đồng ý làm vợ Anh mà ^^ </br> Cảm ơn bé đã đồng ý để cho anh được che chở và bảo vệ bé :3 </br> Anh sẽ hứa đối xử với bé thật tốt và hết lòng iu thương bé ạ :3 </br> Anh iu bé nhìu vl >.< ',
+};
